a43da4edca643b44b9d82337e5838f5f275fb771
Diego Gonzalez <116301465+JummyJoeJackson@users.noreply.github.com>
2025-11-15 21:44:41 -0500

    Add files via upload

---
+++ devtools/.next/dev/server/app/api/reload/route.js
@@ -0,0 +1,6 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/reload/route.js")
+R.c("server/chunks/b2b77_next_4322a930._.js")
+R.c("server/chunks/[root-of-the-server]__0fe2d995._.js")
+R.c("server/chunks/bdc91_devtools__next-internal_server_app_api_reload_route_actions_85294039.js")
+R.m("[project]/my-chesshacks-bot/devtools/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/my-chesshacks-bot/devtools/app/api/reload/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/my-chesshacks-bot/devtools/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/my-chesshacks-bot/devtools/app/api/reload/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
---
+++ devtools/.next/dev/server/chunks/bdc91_devtools__next-internal_server_app_api_reload_route_actions_85294039.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/my-chesshacks-bot/devtools/.next-internal/server/app/api/reload/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=bdc91_devtools__next-internal_server_app_api_reload_route_actions_85294039.js.map
